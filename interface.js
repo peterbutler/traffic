1111337d5d8d4b1372f65560e836bf92a3022671
Peter Butler <peterebutler@gmail.com>
2013-11-23 13:03:34 -0700

    Add toggles for all displays

--- interface.js
+++ interface.js
@@ -1,14 +1,28 @@
 $( document ).ready(function() {
+	// Set up display toggles
+	for( var item_name in window.game.displays ) {
+		console.log(item_name);
+		checked = '';
+		if ( window.game.displays[ item_name ] ){
+			checked = 'checked';
+		}
+		$checkbox = $('<li><input type="checkbox" id="display-' + item_name + '" ' + checked + '/><label for="display-' + item_name + '">' + item_name + '</label></li>');
+		$("#displays ul").append( $checkbox );
+	}
+
+
 	$("#reset").click( function(){
 		reset();
 		return false;
 	});
 
-	$("#display-travelers").change( function(){
+	$("#displays input").change( function(){
+		item_name = $(this).attr('id').split('-')[1];
+		console.log(item_name);
 		if ( $(this).is( ':checked' ) ){
-			window.game.displays.travelers = true;
+			window.game.displays[item_name] = true;
 		} else {
-			window.game.displays.travelers = false;
+			window.game.displays[item_name] = false;
 		}
 	});
 });
