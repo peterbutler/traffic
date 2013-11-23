ef4446feec72d9b19cda4e267993c108c8e765c5
Peter Butler <peterebutler@gmail.com>
2013-11-23 13:35:06 -0700

    Added more interface controls

--- interface.js
+++ interface.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
 	// Set up display toggles
 	for( var item_name in window.game.displays ) {
-		console.log(item_name);
 		checked = '';
 		if ( window.game.displays[ item_name ] ){
 			checked = 'checked';
@@ -9,7 +8,11 @@ $( document ).ready(function() {
 		$checkbox = $('<li><input type="checkbox" id="display-' + item_name + '" ' + checked + '/><label for="display-' + item_name + '">' + item_name + '</label></li>');
 		$("#displays ul").append( $checkbox );
 	}
-
+	$("#traveler-count").val( window.game.traveler_count );
+	$("#places-count").val( window.game.place_count );
+	if ( window.game.single_goal ){
+		$("#single-goal").prop('checked', true);
+	}
 
 
 	$("#reset").click( function(){
@@ -17,13 +20,33 @@ $( document ).ready(function() {
 		return false;
 	});
 
+
+
+
 	$("#displays input").change( function(){
 		item_name = $(this).attr('id').split('-')[1];
-		console.log(item_name);
 		if ( $(this).is( ':checked' ) ){
 			window.game.displays[item_name] = true;
 		} else {
 			window.game.displays[item_name] = false;
 		}
 	});
+
+	$("#traveler-count").change( function(){
+		window.game.traveler_count = $(this).val();
+	})
+	$("#places-count").change( function(){
+		window.game.place_count = $(this).val();
+	})
+	$("#single-goal").change( function(){
+		if ( $(this).is( ':checked' ) ){
+			window.game.single_goal = true;
+		} else {
+			window.game.single_goal = false;
+		}
+	})
 });
+
+
+
+
