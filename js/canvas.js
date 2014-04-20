362af62c8c60c9d14888e42f0d4d504b7265618f
Peter Butler <peterebutler@gmail.com>
2014-04-20 17:34:32 -0600

    Switch cached place sprite to using context object

--- js/canvas.js
+++ js/canvas.js
@@ -1,11 +1,19 @@
-function Canvas( element, width, height){
-	this.width   = width;
-	this.height  = height;
+function Canvas( element, width, height ){
 	this.element = document.getElementById( element );
-	this.context = this.element.getContext("2d");
-	this.element.setAttribute( "width", this.width );
-	this.element.setAttribute( "height", this.height );
+	if ( typeof width === 'undefined' ){
+		this.width = this.element.offsetWidth;
+	} else {
+		this.width   = width;
+		this.element.setAttribute( "width", this.width );
+	}
+	if ( typeof height === 'undefined' ){
+		this.height = this.element.offsetHeight;
+	} else {
+		this.height   = height;
+		this.element.setAttribute( "height", this.height );
+	}
 
+	this.context = this.element.getContext("2d");
 }
 
 Canvas.prototype = {
